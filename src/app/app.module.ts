d21763242d0b04312fc6fb118bbeef221bdd423e
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2020-08-09 01:02:12 +0000

    Création des composants home, contact, about, signin, not-found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/general/home/home.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { AboutComponent } from './modules/general/about/about.component';
+import { SigninComponent } from './modules/general/signin/signin.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    SigninComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
