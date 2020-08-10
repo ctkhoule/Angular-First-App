089b8f07dd62a3aeb43626858fcce7619183be58
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2020-08-10 01:20:57 +0000

    MAJ des composants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+// Importer les modules Routes et RouterModule
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
-
+import { HomeComponent } from './modules/general/home/home.component';
+import { ContactComponent } from './modules/general/contact/contact.component';
+import { AboutComponent } from './modules/general/about/about.component';
+import { SigninComponent } from './modules/general/signin/signin.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+// Configurer les Routes
+const routes: Routes = [
+  {path: '',  component: HomeComponent, },
+  {path: 'contact',  component: ContactComponent, },
+  {path: 'about',  component: AboutComponent, },
+  {path: 'signin',  component: SigninComponent, },
+  {path: '**',  component: NotFoundComponent, }
+];
+// Appeler la méthode forRoot et RooterModule
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-test-app';
+  version = 'Angular version 10.0.8';
 }
