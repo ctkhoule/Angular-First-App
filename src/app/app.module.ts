9e8d91f94725646d8626d6a22716cef41b5fdef8
Cheikh Tidiane KHOULE <ctkhoule@gmail.com>
2020-08-11 04:04:23 +0000

    MAJ du module AppModule et ne lqisser que les composants HomeComponent et NotFoundComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,29 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './modules/general/home/home.component';
-import { ContactComponent } from './modules/general/contact/contact.component';
-import { AboutComponent } from './modules/general/about/about.component';
-import { SigninComponent } from './modules/general/signin/signin.component';
+// import { ContactComponent } from './modules/general/contact/contact.component';
+// import { AboutComponent } from './modules/general/about/about.component';
+// import { SigninComponent } from './modules/general/signin/signin.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
-import { ContactModule } from './modules/general/contact/contact.module';
-import { SigninModule } from './modules/general/signin/signin.module';
-import { AboutModule } from './modules/general/about/about.module';
+// import { ContactModule } from './modules/general/contact/contact.module';
+// import { SigninModule } from './modules/general/signin/signin.module';
+// import { AboutModule } from './modules/general/about/about.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ContactComponent,
-    AboutComponent,
-    SigninComponent,
+    // ContactComponent,
+    // AboutComponent,
+    // SigninComponent,
     NotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ContactModule,
-    SigninModule,
-    AboutModule
+    // ContactModule,
+    // SigninModule,
+    // AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
